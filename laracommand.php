6ca503fc75e5d1cd1adbc4dc0fb50cfa42ac951e
ashwani200dbuglab <87443456+ashwani200dbuglab@users.noreply.github.com>
2021-07-23 11:23:59 +0530

    laracmd

---
+++ laracommand.php
@@ -0,0 +1,24 @@
+composer create-project laravel/laravel –-prefer-dist
+composer create-project laravel/laravel test dev-develop
+php artisan serve
+php artisan config:cache
+php artisan down
+php artisan up
+php artisan make:middleware <middleware-name>
+php artisan make:controller TestController --plain
+use <namespace-name>;
+php artisan app:name SocialNet
+php artisan make:controller MyController
+composer require illuminate/html
+php artisan event:generate
+php artisan make:provider TestFacadesServiceProvider
+interface <contract-name>
+php artisan make:auth
+php artisan route:cache
+php artisan list
+php artisan help serve
+php artisan make:console <name-of-command>           //php artisan make:console DefaultCommand
+php artisan help DefaultCommand
+php artisan key:generate
+php artisan dump-server
+
